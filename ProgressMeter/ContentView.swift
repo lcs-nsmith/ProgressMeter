b2af423f013ed9bcfa25d91a042936458841069b
Nathan Smith <nsmith@lcs.on.ca>
2021-12-15 09:23:36 -0500

    Initial Commit

---
+++ ProgressMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProgressMeter
+//
+//  Created by Nate S on 2021-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProgressMeter/ProgressMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressMeterApp.swift
+//  ProgressMeter
+//
+//  Created by Nate S on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct ProgressMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
